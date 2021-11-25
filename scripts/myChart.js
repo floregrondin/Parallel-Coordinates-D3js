c72df44d27147a401c51bc37708e7c32ea19cf24
Flore GRONDIN <grondinflore@gmail.com>
2021-11-25 21:11:49 +0100

    Affichage des pays sur axes
    Ajouts d'un projet sur iris pour inspirations
    Next commit: Découpler l'affichage selon pays

--- scripts/myChart.js
+++ scripts/myChart.js
@@ -1,28 +1,16 @@
+// set the dimensions and margins of the graph
 var margin = {top: 30, right: 10, bottom: 10, left: 10},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal().rangePoints([0, width], 1),
-        y = {},
-        dragging = {};
-
-var line = d3.svg.line(),
-        axis = d3.svg.axis().orient("left"),
-        background,
-        foreground;
-
-var svg = d3.select("body").append("svg")
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+        .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// Titres des axes
-listeTitresAxes = {
-    "Lieu de naissance":1,
-    "Localisation lors de l'apogée de l'artiste":1,
-    "Dernière localisation connue":1
-};
+        .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
 
 // Nb d'occurences pour chaque pays de naissance d'1 artiste
 cptPaysNaissance = {};
@@ -36,6 +24,13 @@ listeGenres = [];
 genre = [];
 // Pour stocker la liste des artistes récupérés selon leurs genres
 listeArtistesByGenre = [];
+// Titres des axes
+listeTitresAxes = {
+    "Birth": 1,
+    "Famous": 1,
+    "Last known": 1
+};
+
 
 d3.csv("data/data.csv", function (error, data) {
 
@@ -57,7 +52,7 @@ d3.csv("data/data.csv", function (error, data) {
                 // Pour chaque genre dans le tableau
                 for (g in genre) {
                     // Si le genre n'est pas vide et qu'il n'est pas dans la liste
-                    if(genre[g] !== "" && !listeGenres.includes(genre[g])){
+                    if (genre[g] !== "" && !listeGenres.includes(genre[g])) {
                         // On insère dans la liste des genres
                         listeGenres.push(genre[g]);
                     }
@@ -66,105 +61,79 @@ d3.csv("data/data.csv", function (error, data) {
                 listeGenres.push(d.GenreMusique);
             }
         }
-        
+
         // --------- Classer les pays selon la fréquence d'apparition --------- //
         alimenterDico(cptPaysDebutCarriere, d.PaysNaissance);
         alimenterDico(cptPaysNaissance, d.PaysDebutCarriere);
-        
+
         recupererArtisteSelonGenre("Electronic", d);
 
     });
-    
+
     //console.log(cptPaysDebutCarriere);
     //console.log(cptPaysNaissance);
     //console.log(listeArtistesByGenre);
-    
-    // Extract the list of dimensions and create a scale for each.
-    x.domain(dimensions = d3.keys(listeTitresAxes).filter(function (d) {
-        return (y[d] = d3.scale.linear()
-                .domain(d3.extent(data, function (p) {
-                    return +p[d];
+
+
+    // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
+    dimensions = d3.keys(listeTitresAxes);
+
+    // For each dimension, I build a linear scale. I store all in a y object
+    var y = {}
+    for (i in dimensions) {
+        name = dimensions[i]
+        y[name] = d3.scalePoint()
+                .domain(data.map(function(d) { 
+                    return d.PaysNaissance;
                 }))
-                .range([height, 0]));
-    }));
+                .range([height, 0])
+    }
 
-    // Add grey background lines for context.
-    background = svg.append("g")
-            .attr("class", "background")
-            .selectAll("path")
+    // Build the X scale -> it find the best position for each Y axis
+    x = d3.scalePoint()
+            .range([0, width])
+            .padding(1)
+            .domain(dimensions);
+
+    // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+    function path(d) {
+        return d3.line()(dimensions.map(function (p) {
+            return [x(p), y[p](d[p])];
+        }));
+    }
+
+    // Draw the lines
+    svg
+            .selectAll("myPath")
             .data(data)
             .enter().append("path")
-            .attr("d", path);
+            .attr("d", path)
+            .style("fill", "none")
+            .style("stroke", "#69b3a2")
+            .style("opacity", 0.5)
 
-    // Add blue foreground lines for focus.
-    foreground = svg.append("g")
-            .attr("class", "foreground")
-            .selectAll("path")
-            .data(data)
-            .enter().append("path")
-            .attr("d", path);
-
-    // Add a group element for each dimension.
-    var g = svg.selectAll(".dimension")
-            .data(dimensions)
-            .enter().append("g")
-            .attr("class", "dimension")
+    // Draw the axis:
+    svg.selectAll("myAxis")
+            // For each dimension of the dataset I add a 'g' element:
+            .data(dimensions).enter()
+            .append("g")
+            // I translate this element to its right position on the x axis
             .attr("transform", function (d) {
                 return "translate(" + x(d) + ")";
             })
-            .call(d3.behavior.drag()
-                    .origin(function (d) {
-                        return {x: x(d)};
-                    })
-                    .on("dragstart", function (d) {
-                        dragging[d] = x(d);
-                        background.attr("visibility", "hidden");
-                    })
-                    .on("drag", function (d) {
-                        dragging[d] = Math.min(width, Math.max(0, d3.event.x));
-                        foreground.attr("d", path);
-                        dimensions.sort(function (a, b) {
-                            return position(a) - position(b);
-                        });
-                        x.domain(dimensions);
-                        g.attr("transform", function (d) {
-                            return "translate(" + position(d) + ")";
-                        })
-                    })
-                    .on("dragend", function (d) {
-                        delete dragging[d];
-                        transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
-                        transition(foreground).attr("d", path);
-                        background
-                                .attr("d", path)
-                                .transition()
-                                .delay(500)
-                                .duration(0)
-                                .attr("visibility", null);
-                    }));
-
-    // Add an axis and title.
-    g.append("g")
-            .attr("class", "axis")
+            // And I build the axis with the call function
             .each(function (d) {
-                d3.select(this).call(axis.scale(y[d]));
+                d3.select(this).call(d3.axisLeft().scale(y[d]));
             })
+            // Add axis title
             .append("text")
             .style("text-anchor", "middle")
             .attr("y", -9)
             .text(function (d) {
                 return d;
-            });
-
-    // Add and store a brush for each axis.
-    g.append("g")
-            .attr("class", "brush")
-            .each(function (d) {
-                d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
             })
-            .selectAll("rect")
-            .attr("x", -8)
-            .attr("width", 16);
+            .style("fill", "black")
+
 });
 
 /**
@@ -180,7 +149,7 @@ function alimenterDico(dico, paysBd) {
             || (!Object.keys(dico).includes(paysBd))) {
         // Insérer le pays dans le dictionnaire
         dico[paysBd] = 1;
-    // Si pays déjà inséré
+        // Si pays déjà inséré
     } else if (Object.keys(dico).includes(paysBd)) {
         // Incrémenter de 1 le nb d'occurences
         dico[paysBd] += 1;
@@ -192,7 +161,7 @@ function alimenterDico(dico, paysBd) {
  * @param {type} genre Le genre musical
  * @param {type} data La donnée issue du csv (doit comporter les champs GenreMusique et Artiste)
  */
-function recupererArtisteSelonGenre(genre, data){
+function recupererArtisteSelonGenre(genre, data) {
     // Pour stocker les genres compris dans des tableaux de genres dans le csv
     genreIndiv = [];
     // Si le genre est dans un tab dans le csv
@@ -202,46 +171,11 @@ function recupererArtisteSelonGenre(genre, data){
         // Pour chaque genre du tab
         for (g in genreIndiv) {
             // Si le genre n'est pas une chaine de caractères vide et qu'il correspond au genre recherché
-            if (genreIndiv[g] !== "" && genreIndiv[g] === genre){
+            if (genreIndiv[g] !== "" && genreIndiv[g] === genre) {
                 listeArtistesByGenre.push(data.Artiste);
             }
         }
     } else if (data.GenreMusique === genre) {
         listeArtistesByGenre.push(data.Artiste);
     }
-}
-
-function position(d) {
-    var v = dragging[d];
-    return v == null ? x(d) : v;
-}
-
-function transition(g) {
-    return g.transition().duration(500);
-}
-
-// Returns the path for a given data point.
-function path(d) {
-    return line(dimensions.map(function (p) {
-        return [position(p), y[p](d[p])];
-    }));
-}
-
-function brushstart() {
-    d3.event.sourceEvent.stopPropagation();
-}
-
-// Handles a brush event, toggling the display of foreground lines.
-function brush() {
-    var actives = dimensions.filter(function (p) {
-        return !y[p].brush.empty();
-    }),
-            extents = actives.map(function (p) {
-                return y[p].brush.extent();
-            });
-    foreground.style("display", function (d) {
-        return actives.every(function (p, i) {
-            return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-        }) ? null : "none";
-    });
 }
